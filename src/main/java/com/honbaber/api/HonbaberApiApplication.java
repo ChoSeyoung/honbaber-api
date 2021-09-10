7715b9e2f262c04a2586f2ffbe0a0203bac006ac
ChoSeyoung <a01075511157@gmail.com>
2021-09-10 22:56:14 +0900

    first commit

---
+++ src/main/java/com/honbaber/api/HonbaberApiApplication.java
@@ -0,0 +1,13 @@
+package com.honbaber.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonbaberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonbaberApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honbaber/api/HonbaberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honbaber.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonbaberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
