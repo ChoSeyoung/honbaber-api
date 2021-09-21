adb588fad28d863778c818cccc25e58388305a03
ChoSeyoung <a01075511157@gmail.com>
2021-09-21 21:02:55 +0900

    시큐리티 추가

---
+++ src/main/java/com/honbaber/admin/api/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.honbaber.admin.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/api/${API_VERSION}")
+public class AuthController {
+
+}
